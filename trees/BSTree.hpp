d5b292210629519e0c0a562511fbb12d5c01ffce
keanejd <229680459+keanejd@users.noreply.github.com>
2026-03-31 21:33:49 -0500

    The BST code

--- trees/BSTree.hpp
+++ trees/BSTree.hpp
@@ -49,6 +49,38 @@ class BSTree {
             return std::max(height( _node->getLeft() ), height(_node->getRight() )) + 1;
         }
 
+        TreeNode<T> * remove(TreeNode<T> * _node, T _data) {
+            if( _node == nullptr) return nullptr;
+
+            //left subtree
+            if( _node->getData() > _data)
+                _node->setLeft( remove(_node->getLeft(), _data) );
+            //right subtree    
+            
+            else if(_node->getData() < _data)
+                _node->setRight( remove(_node->getRight(), _data) );
+            else {
+                if(_node->getLeft()  == nullptr) {
+                    TreeNode<T> * rightChild = _node->getRight();
+                    delete _node;
+                    return rightChild;    
+                }
+                else if( _node->getRight() == nullptr ) {
+                    TreeNode<T> * leftChild = _node->getLeft();
+                    delete _node;
+                    return leftChild;
+                }
+
+                else {
+                    // leftmost node in the right subtree
+                    TreeNode<T> * temp = findMin( _node->getRight() ) ;
+                    _node->setData(temp->getData());
+                    _node->setRight( remove(temp->getRight(), temp->getData()));
+                }
+            }
+            return _node;
+        }
+
 
     public:
         BSTree() : root(nullptr), nodeCount(0) {}
@@ -66,9 +98,25 @@ class BSTree {
                 return true;
             }
         }
+
+        bool removeNode(const T& _data) {
+            if( contains(_data) ){
+                nodeCount--;
+                root = remove(root, _data);
+                return true;
+            }
+            else    
+                return false;
+        }
         
         bool conatainsData(const T& _data) {
             return contains(root, _data);
         }
-     
+
+        int treeHeight() {
+            return height(root);
+        }
+
+
+        
 };
